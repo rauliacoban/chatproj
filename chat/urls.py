badaaa7ff6cfd66a2f35c797d8fd17549c9c9c15
riacoban <raul.iacoban@lastingsoftware.com>
2023-11-13 19:26:26 +0200

    move urls

--- chat/urls.py
+++ chat/urls.py
@@ -1,8 +1,10 @@
 #chat/urls.py
 from django.urls import path, include
 from . import views
+from chat import consumers
 
 urlpatterns = [
     path("", views.HomeView, name="home"),
-    path("groups/<uuid:uuid>/", views.GroupChatView, name="group")
+    path("groups/<uuid:uuid>/", views.GroupChatView, name="group"),
+    path('ws/some_path/', consumers.JoinAndLeave.as_asgi()),
 ]
--- chatproj/urls.py
+++ chatproj/urls.py
@@ -16,10 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from chat import consumers
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("chat.urls")),
-    path('ws/some_path/', consumers.JoinAndLeave.as_asgi()),
 ]
